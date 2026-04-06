f2c4fcda78420f0cf050eaec1d9cec17e40b1138
Adit Singh <adit@example.com>
2026-04-06 19:18:36 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,25 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+// Dynamic basePath for GitHub Pages logic
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") ?? "";
+  if (repo) {
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+  }
+}
+
 const nextConfig: NextConfig = {
+  output: "export",
+  assetPrefix,
+  basePath,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true, // Required for next/image when using static exports
   },
 };
 
